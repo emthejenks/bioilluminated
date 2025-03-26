be3ac31b2ad6ad7cb5259330ac001e29e91db50c
Emilie Jenkins <emilie.jenkins515@gmail.com>
2025-03-26 18:38:43 -0500

    Restructure GUI File
    Restructured GUI file for easier reading.

--- multiwell_gui.py
+++ multiwell_gui.py
@@ -1,3 +1,10 @@
+###################################################
+# LED Illumination System for Multiwell Plates GUI
+###################################################
+
+######################################
+# Imports
+######################################
 import serial
 import time
 import jsons
@@ -5,44 +12,61 @@ import jsons
 ######################################
 # Global Constants
 ######################################
-VERSION = 1.0
+GUI_VERSION = 1.0
 
-print('LED Illumination System for Multiwell Plates GUI')
-print(f'Version: {VERSION}')
-
-
+######################################
+# Classes
+######################################
 class light:
     def __init__(self, brightness, hue):
        self.brightness = brightness
        self.hue = hue
 
-lightDict = dict()
-for i in range(12):
-    lightDict[i] = light(0,0)
 
-while(True):
-    arduino = serial.Serial('COM3', 9600, timeout=1) # timeout added.
-    time.sleep(2)  # Allow time for the serial port to initialize
+######################################
+# Functions
+######################################
 
-    try:
-        lightNumber = int(input("Gimmie a light number"))
-        brightnessNumber = int(input("Gimmie a brightness"))
-        #hueNumbers = int(input("Gimmie a hue"))
-        hueNumbers = 0
+######################################
+# Main
+######################################
+def main():
+    print('LED Illumination System for Multiwell Plates GUI')
+    print(f'Version: {GUI_VERSION}')
 
-        lightObj = lightDict[lightNumber]
-        lightObj.brightness = brightnessNumber
-        lightObj.hue = hueNumbers
+    lightDict = dict()
+    for i in range(12):
+        lightDict[i] = light(0,0)
+
+    while(True):
+        arduino = serial.Serial('COM3', 9600, timeout=1) # timeout added.
+        time.sleep(2)  # Allow time for the serial port to initialize
+
+        try:
+            lightNumber = int(input("Gimmie a light number"))
+            brightnessNumber = int(input("Gimmie a brightness"))
+            hueNumbers = 0
+
+            lightObj = lightDict[lightNumber]
+            lightObj.brightness = brightnessNumber
+            lightObj.hue = hueNumbers
 
 
-        arduino.write((jsons.dumps(lightDict)).encode())  # Send data to Arduino
-        time.sleep(2)
+            arduino.write((jsons.dumps(lightDict)).encode())  # Send data to Arduino
+            time.sleep(2)
 
-        if arduino.in_waiting > 0 :
-            received_data = arduino.readline().decode().rstrip()
-            print(f"From Arduino: {received_data}")
+            if arduino.in_waiting > 0 :
+                received_data = arduino.readline().decode().rstrip()
+                print(f"From Arduino: {received_data}")
 
 
-    except ValueError:
-        print("i bet you didn't give me numbers, bitch")
-    arduino.close()
+        except ValueError:
+            print("i bet you didn't give me numbers, bitch")
+        arduino.close()
+
+if __name__=="__main__":
+    main()
+
+
+
+
