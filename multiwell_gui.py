9d85c4fd3a88ae8cd12638ecdd942c597059d49e
Emilie Jenkins <emilie.jenkins515@gmail.com>
2025-03-26 19:56:54 -0500

    Create additional LED classes
    Created additional LED classes to hold the entire Array, the
    LEDs with indexes, and the LED settings. This is to better set-up
    the program for future UI improvements including changing the
    settings by row or by column.

--- multiwell_gui.py
+++ multiwell_gui.py
@@ -6,15 +6,75 @@
 # Imports
 ######################################
 import argparse
+from itertools import chain
 import serial
 import sys
 import time
 import jsons
 
+import numpy as np
+
 ######################################
 # Global Constants
 ######################################
 GUI_VERSION = 1.0
+
+LED_CNT = 12
+COL_CNT = 4
+ROW_CNT = 3
+assert COL_CNT*ROW_CNT==LED_CNT, 'Invalid LED array configuration'
+
+######################################
+# Classes
+######################################
+class LedSettings:
+    def __init__(self, brightness=0, hue=0):
+        self.brightness = brightness
+        self.hue = hue
+    def reset(self):
+        self.brightness = 0
+        self.hue = 0
+    def __repr__(self):
+        return f"LedSettings(brightness={self.brightness}, hue={self.hue})"
+
+class Led:
+    def __init__(self, index):
+        self.index = index
+        self.settings = LedSettings()
+    def reset(self):
+        self.settings.reset()
+    def get_settings(self):
+        return self.settings
+    def __repr__(self):
+        return f"Led(index={self.index}, settings={self.settings})"
+
+class LedArray:
+    def __init__(self, rows=ROW_CNT, cols=COL_CNT, led_count=LED_CNT):
+        self.array = [Led(i) for i in range(led_count)]
+    def reset_all(self):
+        for led in self.array:
+            led.reset()
+    def get_led(self, index):
+        if 0 <= index < len(self.array):
+            return self.array[index]
+        else:
+            return None
+    def set_led_settings(self, index, settings):
+      targetLed = self.get_led(index)
+      if targetLed:
+        targetLed.settings = settings
+    def get_tx_array(self):
+        ret_dict = dict()
+        for i,led in enumerate(self.array):
+            ret_dict[i] = led.settings
+        return ret_dict
+    def __repr__(self):
+      return f"LedArray(led_count={len(self.array)})"
+
+######################################
+# Global Variables
+######################################
+mLedArray = LedArray()
 
 ######################################
 # Command Line Arguments
@@ -23,14 +83,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--port', type=str, help='COM port of Arduino ex: COM3', default=None)
 parser.add_argument('--gui_only', action='store_true',help='Disables connecting to Arduino for GUI development.')
 args = parser.parse_args()
-
-######################################
-# Classes
-######################################
-class light:
-    def __init__(self, brightness, hue):
-       self.brightness = brightness
-       self.hue = hue
 
 ######################################
 # Functions
@@ -44,15 +96,10 @@ def main():
     print(f'Version: {GUI_VERSION}')
 
     # Check variables
-
     if not args.gui_only:
         arduino_port = args.port
         if arduino_port is None:
             arduino_port = input("Enter COM port: ")
-
-    lightDict = dict()
-    for i in range(12):
-        lightDict[i] = light(0,0)
 
     while(True):
 
@@ -65,15 +112,15 @@ def main():
                 sys.exit(-1)
             time.sleep(2)  # Allow time for the serial port to initialize
 
-        lightNumber = int(input("Gimmie a light number"))
-        brightnessNumber = int(input("Gimmie a brightness"))
-        hueNumbers = 0
+        # lightNumber = int(input("Light Number: "))
+        # brightnessNumber = int(input("Brightness: "))
+        # hueNumbers = 0
 
-        lightObj = lightDict[lightNumber]
-        lightObj.brightness = brightnessNumber
-        lightObj.hue = hueNumbers
+        # lightObj = lightDict[lightNumber]
+        # lightObj.brightness = brightnessNumber
+        # lightObj.hue = hueNumbers
 
-        json_dump = jsons.dumps(lightDict).encode()
+        json_dump = jsons.dumps(mLedArray.get_tx_array()).encode()
 
         if not args.gui_only:
             try:
@@ -89,6 +136,7 @@ def main():
             arduino.close()
         else:
             print(f'json_dump: {json_dump}')
+        sys.exit()
 
 if __name__=="__main__":
     main()
